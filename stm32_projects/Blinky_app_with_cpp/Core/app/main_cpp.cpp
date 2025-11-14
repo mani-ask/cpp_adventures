e89dc6fb0f13910cfa76ee043df3e5524de85391
manikanta <manikanta.m@tataelxsi.co.in>
2025-11-14 12:05:02 +0530

    Modified the led class to declare constructors, destructors. Compiled and tested on STM32401RE.

--- stm32_projects/Blinky_app_with_cpp/Core/app/main_cpp.cpp
+++ stm32_projects/Blinky_app_with_cpp/Core/app/main_cpp.cpp
@@ -12,37 +12,43 @@
 #include "main_cpp.hpp"
 
 
-class led_class
+led::led( ) : port(GPIOA), pin ((std::uint32_t)0x0020) { };
+
+led::led(port_t port, pin_t pin) : port(port), pin (pin) { };
+
+
+void led::on() const
 {
-	public:
-
-	led_class();
-
-	/* method to blink LED */
-	void blink_led ()
-	{
-		HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
-	}
-};
-
-/* led_class constructor, which does nothing in this example */
-/* It could be used to initialize the object attributes */
-led_class::led_class()
-{
-
+	port->BSRR = pin;
 }
 
+
+void led::off() const
+{
+	port->BSRR = (uint32_t)(pin  <<  16U);
+}
+
+void led::toggle() const
+{
+	uint32_t odr;
+
+	/* get current Output Data Register value */
+	odr = port->ODR;
+
+	/* Set selected pins that were at low level, and reset ones that were high */
+	port->ODR = ((odr & pin) << 16U) | (~odr & pin);
+}
 
 void invoke_cpp_func()
 {
 
 	/* led_class object instantiation */
-	led_class led_green;
+	led led_green;
 
 	while(1)
 	{
 		/* led_class public method access using object */
-		led_green.blink_led();
+		led_green.toggle();
 
 		/* Delay of 500 ms */
 		HAL_Delay(500);
--- stm32_projects/Blinky_app_with_cpp/Core/app/main_cpp.hpp
+++ stm32_projects/Blinky_app_with_cpp/Core/app/main_cpp.hpp
@@ -10,7 +10,34 @@
 
 #include "main.h"
 #include "stm32f4xx_hal_gpio.h"
+#include <cstdint>
 
 void invoke_cpp_func();
+
+class led {
+
+public :
+	typedef GPIO_TypeDef * port_t;
+	typedef std::uint32_t pin_t;
+
+	/* Constructor with no parameter list */
+	led();
+
+	/* Constructor with parameter list */
+	led(port_t port, pin_t pin);
+
+	/* default : tells the compiler to generate a default, empty destructor*/
+	~led() = default;
+
+	/* Class methods */
+	void on () const;
+	void off() const;
+	void toggle() const;
+
+private :
+	const port_t port;
+	const pin_t pin;
+
+};
 
 #endif /* APP_MAIN_CPP_HPP_ */
